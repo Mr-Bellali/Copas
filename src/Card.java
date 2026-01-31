f8fc4e9ddfbe90d7ab9be5629257fa14f11e68ed
Yassine Bellali <bellali.yassine52@gmail.com>
2026-01-31 23:16:04 +0100

    first commit

---
+++ src/Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    static int id = 0;
+    private String type;
+    private int number;
+
+    Card(String type, int number){
+        this.type = type;
+        this.number = number;
+        id++;
+    }
+
+    public void displayInfo(){
+        IO.println("type: " + this.type);
+        IO.println("number: " + this.number);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+void main() {
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.setTitle("Carta");
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+
+}
