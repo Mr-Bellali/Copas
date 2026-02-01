f72b2f962159c099346a1a62a146b6e15588494c
Yassine Bellali <bellali.yassine52@gmail.com>
2026-02-01 01:42:25 +0100

    added a new sleep methode to update the game on 60 fps

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,10 +12,13 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    //    Game's frame rate
+    int FPS = 60;
+
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
 
-//    Set player's default position
+    //    Set player's default position
     int playerX = 100;
     int playerY = 100;
     int playerSpeed = 4;
@@ -35,51 +38,48 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        double drawInterval = 1000000000 / FPS; // 0.0166666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while (gameThread != null) {
-//        1 Update
+//          1 Update
             update();
 
-//        2 Draw the screen with updated information
+//          2 Draw the screen with updated information
             repaint();
+
+            try {
+                double remainingTime = (nextDrawTime - System.nanoTime()) / 1000000;
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
     }
 
     //    Methode to update the frame
     public void update() {
-        if(keyHandler.upPressed){
+        if (keyHandler.upPressed) {
             playerY -= playerSpeed;
-            try {
-                Thread.sleep(playerSpeed);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
-        if(keyHandler.downPressed){
+        if (keyHandler.downPressed) {
             playerY += playerSpeed;
-            try {
-                Thread.sleep(playerSpeed);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
 
         }
-        if(keyHandler.leftPressed){
+        if (keyHandler.leftPressed) {
             playerX -= playerSpeed;
-            try {
-                Thread.sleep(playerSpeed);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
 
         }
-        if(keyHandler.rightPressed){
+        if (keyHandler.rightPressed) {
             playerX += playerSpeed;
-            try {
-                Thread.sleep(playerSpeed);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
 
         }
     }
@@ -87,10 +87,10 @@ public class GamePanel extends JPanel implements Runnable {
     //    Methode to draw
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize , tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 }
